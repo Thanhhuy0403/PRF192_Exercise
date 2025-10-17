87b9c898e0e961a3f2c75b987f412878c9419017
Thanhhuy0403 <huythanhphan2020fpt@gmail.com>
2025-10-18 00:00:08 +0700

    do oop exercise 4

--- Student.cpp
+++ Student.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 using namespace std;
-
 class Student
 {
 private:
     string name;
     int age;
+    string gender;
+    double gpa;
 
 public:
-    Student() {}
-    void getInformation() {}
-    void display() {}
+    Student(string name, int age, string gender, double gpa)
+    {
+        this->name = name;
+        this->age = age;
+        this->gender = gender;
+        this->gpa = gpa;
+    }
+    void display()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Gender: " << gender << endl;
+        cout << "GPA: " << gpa << endl;
+    }
 };
