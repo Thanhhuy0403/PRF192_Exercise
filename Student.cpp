bc70d83f3a523fac464a3e4104bb8546e5a5fa02
Thanhhuy0403 <huythanhphan2020fpt@gmail.com>
2025-10-17 23:50:05 +0700

    add Student.cpp

---
+++ Student.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+private:
+    string name;
+    int age;
+
+public:
+    Student() {}
+    void getInformation() {}
+    void display() {}
+};
