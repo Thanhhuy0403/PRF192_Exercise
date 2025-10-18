645cee823774ed448f2d73ed60d4e8447bcc8af1
Thanhhuy0403 <huythanhphan2020fpt@gmail.com>
2025-10-18 07:56:37 +0700

    add bai 11 oop

--- Student.cpp
+++ Student.cpp
@@ -1,26 +1,31 @@
-#include <iostream>
+#include "Person.cpp"
+
 using namespace std;
-class Student
+
+class Student : public Person
 {
 private:
-    string name;
-    int age;
-    string gender;
     double gpa;
 
 public:
-    Student(string name, int age, string gender, double gpa)
+    Student(string name, int age, string address, double gpa) : Person(name, age, address)
     {
-        this->name = name;
-        this->age = age;
-        this->gender = gender;
         this->gpa = gpa;
     }
+
+    double getGpa()
+    {
+        return gpa;
+    }
+
+    void setGpa(double gpa)
+    {
+        this->gpa = gpa;
+    }
+
     void display()
     {
-        cout << "Name: " << name << endl;
-        cout << "Age: " << age << endl;
-        cout << "Gender: " << gender << endl;
+        Person::display();
         cout << "GPA: " << gpa << endl;
     }
 };
